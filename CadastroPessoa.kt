a0d26f050f43534d53d5a7704fafa442f018355b
AquiniG <aquini94@gmail.com>
2026-09-14 18:29:29 -0300

    Adiciona cadastro de pessoa com múltiplos tipos de dados

--- CadastroPessoa.kt
+++ CadastroPessoa.kt
@@ -4,33 +4,31 @@ fun main() {
     val nome = readln()
 
     println("Digite sua idade:")
-    val idade = readln()
+    val idade = readln().toInt()
 
     println("Digite sua altura em metros (ex:1.75):")
-    val alturaMetros = readln()
+    val alturaMetros = readln().toDouble()
 
     println("Digite o salário pretendido:")
-    val salarioPretendido = readln()
+    val salarioPretendido = readln().toFloat()
 
     println("Já trabalhou antes? (s/n)")
-    val jaTrabalhou = readln()
+    val resposta = readln()
+    var jaTrabalhou: Boolean = resposta == "s"
 
-    println("Inicial do nome:")
-    val inicial = readln()
-
-
+    val inicial = nome[0]
 
 
 
     println()
     println("---Cadastro Realizado---")
     println()
-    println("Nome: $nome")
-    println("Idade: $idade")
-    println("Altura: $alturaMetros")
-    println("Salário pretendido: $salarioPretendido")
-    println("Já trabalhou antes? $jaTrabalhou")
-    println("Inicial do nome: $inicial")
+    println("Nome: $nome (${nome::class.simpleName})")
+    println("Idade: $idade (${idade::class.simpleName})")
+    println("Altura: ${alturaMetros}m (${alturaMetros::class.simpleName})")
+    println("Salário pretendido: $salarioPretendido (${salarioPretendido::class.simpleName})")
+    println("Já trabalhou antes? $jaTrabalhou (${jaTrabalhou::class.simpleName})")
+    println("Inicial do nome: $inicial (${inicial::class.simpleName})")
 
 
 
