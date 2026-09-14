0241ead954e9cbcd333c950e27825f252e121732
AquiniG <aquini94@gmail.com>
2026-09-14 16:10:19 -0300

    Adicionado cadastro básico do usuário e impressão na tela

---
+++ CadastroPessoa.kt
@@ -0,0 +1,38 @@
+fun main() {
+
+    println("Digite seu nome:")
+    val nome = readln()
+
+    println("Digite sua idade:")
+    val idade = readln()
+
+    println("Digite sua altura em metros (ex:1.75):")
+    val alturaMetros = readln()
+
+    println("Digite o salário pretendido:")
+    val salarioPretendido = readln()
+
+    println("Já trabalhou antes? (s/n)")
+    val jaTrabalhou = readln()
+
+    println("Inicial do nome:")
+    val inicial = readln()
+
+
+
+
+
+    println()
+    println("---Cadastro Realizado---")
+    println()
+    println("Nome: $nome")
+    println("Idade: $idade")
+    println("Altura: $alturaMetros")
+    println("Salário pretendido: $salarioPretendido")
+    println("Já trabalhou antes? $jaTrabalhou")
+    println("Inicial do nome: $inicial")
+
+
+
+
+}
